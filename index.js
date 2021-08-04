064523bb6a29e50ecd6798689d50792a0d40ba78
khreate <surajkhosla79@gmail.com>
2021-08-04 17:12:31 -0500

    added GET route to get math problems from the API - /getProblem

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+/* 
+ * This api route GETS a random math problem from the math problem api.
+*/
 
+const axios = require('axios');
+const { response } = require('express');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/getProblem', function(req, res) {
+    axios.get('https://codubee-projects-api.herokuapp.com/math/getProblem')
+    .then(function(response) {
+        console.log(response["data"])
+        res.json(response["data"])
+    })
+    .catch(function(error) {
+        console.log(error)
+        response.json({"message": "ERROR: REQUEST FAILED"})
+    })
+});
+
+app.listen(8080, function() {
+    console.log("listening")
+})
