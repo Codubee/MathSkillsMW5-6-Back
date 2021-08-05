72498bc05d7721504f430c347a09fc3bcc10b569
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-08-05 16:24:42 -0500

    MB-9  (#4)
    * first commit
    
    * get route that gets the users matches
    
    * updated server start script
    
    * Added new listen and start script to package.json
    
    * Added new file
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+
+// get route that gets the users matches
+app.get('/getMatches', (req, res) => {
+    axios.get(`https://codubee-projects-api.herokuapp.com/math/getMatches?userId=${req.query.userId}`)
+    .then((axiosResponse) => {
+        res.json(axiosResponse.data)
+    })
+    .catch((err) => {
+        res.json(err);
+    })
+});
+
+app.listen( process.env.PORT || 8080, () => console.log('Example app listening at http://localhost:8080'))
 
