f69f5c9b5ea9f89b14be7cd7dd4a9e08c03cb00a
salmanh123 <salmanh084@gmail.com>
2021-08-06 17:37:40 -0500

    Added POST request route that posts to API that contains math problems.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 
+const express = require('express');
+const {default: axios} = require('axios');
+const app = express();
+
+app.use(express.json())
+
+/*
+    Usage:
+    1. Open Postman
+    2. Select Post.
+    3. Call it with localhost:8080/
+    4. After the '/' specify math/addProblem
+    5. For POST request in Postman, go to the body tab, select JSON on the dropdown, then in the
+    text area create a json object with the keys "userID" & "problem". Within "problem" specify a 
+    object with {} add in the keys "problem", "answer", & "problemId"
+    8. For each call, you will be sent back a response indicating success or failure
+
+    Example request: 
+    {
+    "userId":"99",
+    "problem": {"problem":"5+2", "answer":"4","problemId":"3"}
+    }
+*/
+
+// Makes post request to /math/addProblem route
+app.post('/math/addProblem', (pmanReq,pmanRes)=>{
+    
+    // Grab the posts body
+    const bodyPost = pmanReq.body
+    // Make post request to our API
+    axios.post("https://codubee-projects-api.herokuapp.com/math/addProblem",
+    bodyPost).then(function(newResponse){
+        // Executes if request was successful
+        console.log(newResponse)
+        pmanRes.json({"message":"Posted"})
+    }).catch(function(err){
+        // Executes if there was any errors
+        console.log(err)
+        pmanRes.json({"message":"Error"})
+    })
+})
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Listening on port 8080.")
+})
