ced5ec8c1b00313c7f27bd2f2c2b8a3575cf37cb
Salman Hossain <64250542+salmanh123@users.noreply.github.com>
2021-08-11 17:22:29 -0500

    [MB-7] Create a post route to add a math problem and a user to the db. (#7)
    * Added POST request route that posts to API that contains math problems.
    
    * Changed route to addProblem
    
    * Changed comments to reflect changed route
    
    * Fixed spacing
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+/*
+    Usage:
+    1. Open Postman
+    2. Select Post.
+    3. Call it with localhost:8080/
+    4. Call the /addProblem route
+    5. For POST request in Postman, go to the body tab, select JSON on the dropdown, then in the
+    text area create a json object with the keys "userID" & "problem". Within "problem" specify a 
+    object with {} add in the keys "problem", "answer", & "problemId"
+    8. For each call, you will be sent back a response indicating success or failure
+
+    Example request: 
+    {
+    "userId":"99",
+    "problem": {"problem":"5+2", "answer":"4","problemId":"3"}
+    }
+*/
+
+// Makes post request to /addProblem route
+app.post('/addProblem', (pmanReq,pmanRes)=>{
+    
+    // Grab the posts body
+    const bodyPost = pmanReq.body
+    // Make post request to our API
+    axios.post("https://codubee-projects-api.herokuapp.com/math/addProblem",bodyPost)
+    .then(function(newResponse){
+        // Executes if request was successful
+        console.log(newResponse)
+        pmanRes.json({"message":"Posted"})
+    }).catch(function(err){
+        // Executes if there was any errors
+        console.log(err)
+        pmanRes.json({"message":"Error"})
+    })
+})
+
 app.get('/getProblem', function(req, res) {
     axios.get('https://codubee-projects-api.herokuapp.com/math/getProblem')
     .then(function(response) {
