b25c7312d9c087ee9a53759d2ab228557c3c645a
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-08-16 10:34:30 -0500

    added yelp search events

--- JM/index.js
+++ JM/index.js
@@ -1,8 +1,17 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+require('dotenv').config({ path: '../.env' });
 
 app.use(express.json());
+app.use(cors());
+
+const corsOptions = {
+    origin: 'https://www.yelp.com/developers/documentation/v3/event_search',
+    optionsSuccessStatus: 200
+}
 
 app.post('/addPerson', (req, res) => {
     const body = req.body;
@@ -35,6 +44,39 @@ app.delete('/deletePerson', (req, res) => {
         res.status(500).json({"Message": "Error"});
     })
 });
+
+app.get('/events', (req, res) => {
+    let params = Object.keys(req.query).length === 0 ? '' : req.query;
+    console.log(params);
+    console.log(req.query);
+
+    axios.get(`https://api.yelp.com/v3/events`, {
+        headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+        },
+        params: {
+            location: params.location ? params.location : null,
+            limit: params.limit ? params.limit : null,
+            locale: params.locale ? params.locale : null,
+            offset: params.offset ? params.offset : null,
+            sort_by: params.sort_by ? params.sort_by : null,
+            sort_on: params.sort_on ? params.sort_on : null,
+            start_date: params.start_date ? params.start_date : null,
+            end_date: params.end_date ? params.end_date : null,
+            categories: params.categories ? params.categories : null,
+            is_free: params.is_free ? params.is_free : null,
+            latitude: params.latitude ? params.latitude : null,
+            radius: params.radius ? params.radius : null,
+            excluded_events: params.excluded_events ? params.excluded_events : null
+        }
+    })
+    .then((response) => {
+        res.json(response.data);
+    })
+    .catch((err) => {
+        res.json(err);
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is running');
