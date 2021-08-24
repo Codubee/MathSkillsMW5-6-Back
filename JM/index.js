8661817dd2f209a88e3ac3fb43e9626cb17dd79b
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-08-24 12:42:06 -0500

    MB-12, MB-13, MB-14 (#10)
    * added yelp search events
    
    * removed unused code
    
    * added new param
    
    * removed console.logs used for debugging
    
    * added business search from yelp api
    
    * added eventsDetails route
    
    * added id to url

--- JM/index.js
+++ JM/index.js
@@ -1,8 +1,12 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+require('dotenv').config({ path: '../.env' });
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/addPerson', (req, res) => {
     const body = req.body;
@@ -33,6 +37,89 @@ app.delete('/deletePerson', (req, res) => {
     })
     .catch(() => {
         res.status(500).json({"Message": "Error"});
+    })
+});
+
+app.get('/businessSearch', (req, res) => {
+    let params = Object.keys(req.query).length === 0 ? '' : req.query;
+
+    axios.get(`https://api.yelp.com/v3/businesses/search`, {
+        headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+        },
+        params: {
+            term: params.term ? params.term : null,
+            location: params.location ? params.location : null,
+            latitude: params.latitude ? params.latitude : null,
+            longitude: params.longitude ? params.longitude : null,
+            radius: params.radius ? params.radius : null,
+            categories: params.categories ? params.categories : null,
+            locale: params.locale ? params.locale : null,
+            limit: params.limit ? params.limit : null,
+            offset: params.offset ? params.offset : null,
+            sort_by: params.sort_by ? params.sort_by : null,
+            price: params.price ? params.price : null,
+            open_now: params.open_now ? params.open_now : null,
+            open_at: params.open_at ? params.open_at : null,
+            attributes: params.attributes ? params.attributes : null
+        }
+    })
+    .then((response) => {
+        res.json(response.data);
+    })
+    .catch((err) => {
+        res.json(err);
+    })
+});
+
+app.get('/events', (req, res) => {
+    let params = Object.keys(req.query).length === 0 ? '' : req.query;
+
+    axios.get(`https://api.yelp.com/v3/events`, {
+        headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+        },
+        params: {
+            locale: params.locale ? params.locale : null,
+            offset: params.offset ? params.offset : null,
+            limit: params.limit ? params.limit : null,
+            sort_by: params.sort_by ? params.sort_by : null,
+            sort_on: params.sort_on ? params.sort_on : null,
+            start_date: params.start_date ? params.start_date : null,
+            end_date: params.end_date ? params.end_date : null,
+            categories: params.categories ? params.categories : null,
+            is_free: params.is_free ? params.is_free : null,
+            location: params.location ? params.location : null,
+            latitude: params.latitude ? params.latitude : null,
+            longitude: params.longitude ? params.longitude : null,
+            radius: params.radius ? params.radius : null,
+            excluded_events: params.excluded_events ? params.excluded_events : null
+        }
+    })
+    .then((response) => {
+        res.json(response.data);
+    })
+    .catch((err) => {
+        res.json(err);
+    })
+});
+
+app.get('/eventDetails', (req, res) => {
+    let params = Object.keys(req.query).length === 0 ? '' : req.query;
+    
+    axios.get(`https://api.yelp.com/v3/events/${params.id}`, {
+        headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+        },
+        params: {
+            locale: params.locale ? params.locale : null,
+        }
+    })
+    .then((response) => {
+        res.json(response.data);
+    })
+    .catch((err) => {
+        res.json(err);
     })
 });
 
